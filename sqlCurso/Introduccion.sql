a5236262baac67743a568c79fcae68e03480081b
Javier Ramirez <javierramirez1230123@gmail.com>
2023-05-10 13:16:39 -0300

    update

---
+++ sqlCurso/Introduccion.sql
@@ -0,0 +1,29 @@
+-- CREATE DATABASE tienda;
+
+--USE tienda;
+
+--CREATE TABLE productos (
+--	id INT PRIMARY KEY,
+--	nombre VARCHAR(50),
+--	precio DECIMAL(10,2),
+--	stock INT
+--);
+
+--CREATE TABLE clientes(
+--	id INT PRIMARY KEY,
+--	nombre VARCHAR(50), -- almacena 50 caracteres
+--	direccion VARCHAR(100), -- almacena 100 caracteres
+--	telefono VARCHAR(15)
+--)
+
+--para ver todo los datos de la tabla
+--USE tienda;
+
+--SELECT * FROM productos;
+
+--Ejercicio > Insertar un nuevo producto en la tabla productos
+
+--USE tienda;
+
+--INSERT INTO productos(id, nombre, precio, stock)
+--VALUES(1, 'Camiseta', 25.45, 40);
